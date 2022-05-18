7a6ab12335fc55a621c57cc28e2942eae8384091
Austin <austin.jiuk.kim@mgmail.com>
2022-05-18 18:50:44 +0900

    ,

---
+++ Assignments/Session10_HW/Session10/project/app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
